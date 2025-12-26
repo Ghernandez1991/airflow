6379e7bd76f9a63903531dd4052daf5a512213da
Ghernandez1991 <hernandez.gabriel59@yahoo.com>
2025-12-26 13:39:57 -0600

    adding a first commit

---
+++ dags/one_hundred_hello_world.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import logging
+
+logger = logging.getLogger("airflow.task")
+
+
+def hello_world():
+
+    logging.info("hello world")
+
+
+with DAG(
+    dag_id="hello_100_tasks",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["k8s-demo"],
+) as dag:
+
+    previous = None
+
+    for i in range(1, 101):
+        t = PythonOperator(
+            task_id=f"hello_task_{i}",
+            python_callable=hello_world,
+        )
+
+        if previous:
+            previous >> t
+        previous = t
